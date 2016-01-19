b2e178983a17bb74eb71998b746ac3112e33b009
Alex Anschuetz <alexanschuetz@gmail.com>
2016-01-19 15:08:56 -0800

    creates index and create functions in controller

---
+++ controllers/todos_controller.js
@@ -0,0 +1,21 @@
+var Todo = require('../models/todo.js')
+
+function index (req, res) {
+	Todo.find({}, function(err, todos) {
+		if (err) console.log(err)
+			res.render('layout', {todos: todos})
+	})
+}
+
+function create (req, res, next) {
+	var todo = new Todo ()
+	todo.title = req.body.title
+	todo.description = req.body.description
+	todo.priority = req.body.priority
+	todo.date = req.body.date
+
+	todo.save(function (err, todo, count) {
+		if(err) console.log(err)
+		res.redirect('/todos')
+	})
+}
