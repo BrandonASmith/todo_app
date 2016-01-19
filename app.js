6110001bf7ba2375c1680cf6c6983a9834a8deb4
Brandon Smith <basmith808@gmail.com>
2016-01-19 15:25:58 -0800

    Sets up app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var express 	= require('express'),
+	path 		= require('path'),
+	logger 		= require('morgan'),
+	bodyParser  = require('body-parser'),
+	app 		= require('express'),
+	mongoose	= require('mongoose'),
+	port 		= process.env.PORT || 3000,
+	todoRouter  = ('./routes/todo_routes.js')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.listen(port)
+console.log('The server is running on port' + port)
