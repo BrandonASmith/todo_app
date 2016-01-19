107fd6c379c65a388ff72b9b5d24774ef7631687
TJ Palazzari <tjpalazzari@yahoo.com>
2016-01-19 15:09:28 -0800

    adds main routes

---
+++ routes/todo_routes.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+	express = require('express'),
+	todoRouter = express.Router(),
+	bodyParser = require('body-parser')
+
+todoRouter.route('/todos')
+	.get(todosController.index)
+	.post(todosController.create)
+
+todoRouter.route('/todos/:id')
+	.put(todosController.update)
+	.get(todosController.show)
+	.delete(todosController.destroy)
